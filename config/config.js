a9095231f192ef5ca7eda3e8c351bdef96b5adfe
emilkip <emil@cloudcoreo.com>
2017-11-27 21:56:46 +0600

    Fixed connection url

--- config/config.js
+++ config/config.js
@@ -15,9 +15,9 @@ module.exports = {
       }
     },
     db: {
-      url: `mongodb://${process.env.MONGODB_USERNAME}:
-            ${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:
-            ${process.env.MONGODB_PORT}/${process.env.MONGODB_DB}`
+      url: `mongodb://${process.env.MONGODB_USERNAME}:` + 
+            `${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:` +
+            `${process.env.MONGODB_PORT}/${process.env.MONGODB_DB}`
     }
   },
   development: {
